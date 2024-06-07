be1ae8f77eebb970d426cf1a10d0df9d19a4b421
rgfle <rgfle@DESKTOP-M14RMBO>
2024-06-07 19:50:51 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp6/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tatar
+{
+    public class WordDetectedException : Exception
+    {
+        public WordDetectedException(string message) : base(message)
+        {
+
+        }
+    }
+}
---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tatar;
+
+namespace ConsoleApp6
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите сообщение: ");
+            string message = Console.ReadLine();
+            Console.Write("Введите слово для поиска: ");
+            string searchWord = Console.ReadLine();
+
+            try
+            {
+                if (message.Contains(searchWord))
+                {
+                    throw new WordDetectedException($"Обнаружено слово: {searchWord}");
+                }
+                else
+                {
+                    Console.WriteLine("Слово не обнаружено.");
+                }
+            }
+            catch (WordDetectedException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Произошла ошибка: {ex.Message}");
+            }
+            finally
+            {
+                Console.WriteLine($"Сообщение: {message}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+
+}
+
